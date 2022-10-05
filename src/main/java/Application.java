27cd0062e0f6cc7896ad2fb6255959e4555d0184
Lucas Nakajima <nakajima.lucas02@gmail.com>
2022-10-05 17:47:47 +0100

    CHanges made in step 5, waiting for teacher response

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,5 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -6,12 +8,23 @@ import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
 
 public class Application {
-    public static void main(String[] args) throws IOException {
-        Terminal terminal = new
-                DefaultTerminalFactory().createTerminal();
-        Screen screen = new TerminalScreen(terminal);
-        screen.setCursorPosition(null);
-        screen.startScreen();
-        screen.doResizeIfNecessary();
+    public static void main(String[] args) {
+        try {
+            TerminalSize terminalSize = new TerminalSize(90, 20);
+            DefaultTerminalFactory terminalFactory = new
+                    DefaultTerminalFactory()
+                    .setInitialTerminalSize(terminalSize);
+            Terminal terminal = terminalFactory.createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null);
+            screen.startScreen();
+            screen.doResizeIfNecessary();
+            screen.clear();
+            screen.setCharacter(10, 10, TextCharacter.fromCharcter('X')[0]);
+            screen.refresh();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
