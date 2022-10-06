74d547b72b1307e8a1ec753b9476a56a133520d1
Lucas Nakajima <nakajima.lucas02@gmail.com>
2022-10-06 10:32:56 +0100

    Finished step 6

---
+++ src/main/java/Game.java
@@ -0,0 +1,48 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Game {
+    private Screen screen;
+    private int x = 10;
+    private int y = 10;
+
+    Game() {
+        try {
+            TerminalSize terminalSize = new TerminalSize(40, 20);
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+            Terminal terminal = terminalFactory.createTerminal();
+            screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null); // we don't need a cursor
+            screen.startScreen(); // screens must be started
+            screen.doResizeIfNecessary(); // resize screen if necessary
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void draw() throws IOException {
+        screen.clear();
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
+        screen.refresh();
+    }
+
+
+    public void run() {
+        try {
+                draw();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
