924d7e928ca27be23d2eda681d7863576554be3a
Lucas Nakajima <nakajima.lucas02@gmail.com>
2022-10-06 13:58:01 +0100

    Created Arena

---
+++ src/main/java/Arena.java
@@ -0,0 +1,27 @@
+public class Arena {
+    private int height;
+    private int width;
+
+    Arena(int h, int w){
+        this.height = h;
+        this.width = w;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+
+}
